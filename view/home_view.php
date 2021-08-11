0ef46c48c5a12f1e62e0625c8074dbbe1b4ab01d
Benoît Cordier <bca.cordier@gmail.com>
2021-08-11 15:48:49 +0200

    --

--- view/home_view.php
+++ view/home_view.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8" />
         <title>Projet 4</title>
-        <link href="public/style.css" rel="stylesheet" />
+        <link href="public/css/style.css" rel="stylesheet" />
     </head>
 
     <body>
