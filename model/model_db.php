be81b89f1c5bfebda7ab95adadd3dcaaf519f986
Benoît Cordier <bca.cordier@gmail.com>
2022-02-05 11:10:26 +0100

    Ajustement Prod

--- model/model_db.php
+++ model/model_db.php
@@ -2,7 +2,7 @@
 function dbConnection()
 {
     try {
-        return new PDO('mysql:host=localhost;dbname=project_4;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        return new PDO('mysql:host=db5006527767.hosting-data.io;dbname=dbs5416493;charset=utf8', 'dbu905972', '2308040691', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     } catch (Exception $e) {
         die('Erreur : '.$e->getMessage());
     }
